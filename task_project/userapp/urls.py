81ab52c42893e285932c491530beedc99e4641c5
Phan Công Chiến <phancongchien8278@gmail.com>
2025-03-14 23:31:25 +0700

    Update urls.py

--- task_project/userapp/urls.py
+++ task_project/userapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'userapp'
+
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]
