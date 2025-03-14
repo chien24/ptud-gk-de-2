b3e24fdca0d8629ffe818f915f9d767915366488
Phan Công Chiến <phancongchien8278@gmail.com>
2025-03-14 23:30:20 +0700

    Update views.py

--- task_project/userapp/views.py
+++ task_project/userapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from taskapp.models import Category
 from .models import Profile
